5a2b7c298ed3f3c4971da320e36f49bc709f0889
cldborges <cldborges@yahoo.com.br>
2022-02-25 12:36:37 -0300

    renomear arquivos de video

--- limpar plex.py
+++ limpar plex.py
@@ -22,7 +22,7 @@ for tipo in tipos:
             for arquivo in os.listdir(caminho_completo):
                 caminho_arquivo = os.path.join(caminho_completo, arquivo)
                 if os.path.getsize(caminho_arquivo) < 200000 * 1024 and 'srt' not in (arquivo):
-                    print(arquivo)
+                    #print(arquivo)
                     os.chmod(caminho_arquivo, 0o777)
                     #print(caminho_arquivo, os.path.getsize(caminho_arquivo))
                     try:
@@ -35,5 +35,10 @@ for tipo in tipos:
                 nome_pasta = nome_pasta.replace(palavra, '')
                 nome_pasta = nome_pasta.replace('  ', ' ')
                 nome_pasta = nome_pasta.strip()
-            os.rename(caminho_completo, os.path.join(raiz,nome_pasta))
-
+            caminho_novo = os.path.join(raiz,nome_pasta)
+            os.rename(caminho_completo, caminho_novo)
+            for arquivo_video in os.listdir(caminho_novo):
+                if os.path.isfile(os.path.join(caminho_novo, arquivo_video)):
+                    #print(arquivo_video)
+                    os.rename(os.path.join(caminho_novo, arquivo_video),(os.path.join(caminho_novo, nome_pasta) + arquivo_video[-4:]))
+                    #print(os.path.join(caminho_novo, nome_pasta) + arquivo_video[-4:])
