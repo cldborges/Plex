c44fa746226b623f93e80b945566c9da83cb8c06
cldborges <cldborges@yahoo.com.br>
2022-06-27 19:53:04 -0300

    Limpar Séries criado

---
+++ limpar series.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import os
+import re
+
+pastas_base = []
+pastas_a_corrigir = []
+i = 0
+#drives = [r'C:\Users\z0026jjc\OneDrive - abc\Séries'] #Siemens
+#drives = [r'C:\Users\Claudio\OneDrive - abc\Séries', r'C:\Users\Claudio\OneDrive - abc (1)\Filmes\Series'] #Notebook pessoal
+drives = [r'C:\Onedrives\OneDrive - abc\Séries', r'D:\OneDrives\OneDrive - abc\Filmes\Series'] #PC Casa
+pastas = os.listdir(drives[i])
+for pasta in pastas:
+#    x = re.search('^[A-Z].*[a-z]$', pasta)
+# pesquisa no nome da pasta
+    x = re.search('^.*((\.)|(Temporada)).*$', pasta)
+    excessoes = re.search('sync.ffs_db', pasta)
+    if x == None:
+        pastas_base.append(pasta)
+    elif excessoes != None:
+        pass
+    else:
+        pastas_a_corrigir.append(pasta)
+for pasta_a_corrigir in pastas_a_corrigir:
+    for pasta_base in pastas_base:
+        if pasta_base in pasta_a_corrigir or pasta_base in pasta_a_corrigir.replace('.', ' '):
+            caminho_completo = os.path.join(drives[i], pasta_a_corrigir)
+            caminho_completo_novo = os.path.join(drives[i], pasta_base)
+            #print(caminho_completo)
+            for arquivo in os.listdir(caminho_completo):
+                if os.path.getsize(os.path.join(caminho_completo, arquivo)) > 200000 * 1024 or 'srt' in (arquivo):
+                    print(os.path.join(caminho_completo, arquivo), ' para ', os.path.join(caminho_completo_novo, arquivo))
+                    os.replace(os.path.join(caminho_completo, arquivo), os.path.join(caminho_completo_novo, arquivo))
+                else:
+                    os.chmod(os.path.join(caminho_completo, arquivo), 0o777)
+                    os.remove(os.path.join(caminho_completo, arquivo))
+            os.rmdir(caminho_completo)
+            #os.chmod(caminho_completo, 0o777)
+            #os.remove(caminho_completo)
+
+#print(pastas_base)
+#print(pastas_a_corrigir)
