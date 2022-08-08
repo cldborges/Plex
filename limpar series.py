991abf3f2a4c17028be92d757e32b506a252e67e
cldborges <cldborges@yahoo.com.br>
2022-08-08 12:41:38 -0300

    Apagadas informações desnecessárias

--- limpar series.py
+++ limpar series.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
 import os
 import re
 
@@ -10,11 +9,10 @@ pastas_a_corrigir = []
 #i = 1
 #drives = [r'C:\Users\z0026jjc\OneDrive - abc\Séries'] #Siemens
 #drives = [r'C:\Users\Claudio\OneDrive - abc\Séries', r'C:\Users\Claudio\OneDrive - abc (1)\Filmes\Series'] #Notebook pessoal
-drives = [r'C:\Onedrives\OneDrive - abc\Séries', r'D:\OneDrives\OneDrive - abc\Filmes\Series'] #PC Casa
+drives = [r'C:\Onedrives\OneDrive - abc\Séries', r'D:\OneDrives\OneDrive - abc\Séries'] #PC Casa
 for drive in drives:
     pastas = os.listdir(drive)
     for pasta in pastas:
-    #    x = re.search('^[A-Z].*[a-z]$', pasta)
     # pesquisa no nome da pasta se tem ponto ou a palavra depois do |
         x = re.search('^.*((\.)|(Temporada)).*$', pasta)
         excessoes = re.search('sync.ffs_db', pasta)
@@ -29,7 +27,6 @@ for drive in drives:
             if pasta_base in pasta_a_corrigir or pasta_base in pasta_a_corrigir.replace('.', ' '):
                 caminho_completo = os.path.join(drive, pasta_a_corrigir)
                 caminho_completo_novo = os.path.join(drive, pasta_base)
-                #print(caminho_completo)
                 for arquivo in os.listdir(caminho_completo):
                     if os.path.getsize(os.path.join(caminho_completo, arquivo)) > 200000 * 1024 or 'srt' in (arquivo):
                         print(os.path.join(caminho_completo, arquivo), ' para ', os.path.join(caminho_completo_novo, arquivo))
@@ -41,8 +38,3 @@ for drive in drives:
                 #os.chmod(caminho_completo, 0o777)
                 #os.remove(caminho_completo)
     pastas_a_corrigir.clear()
-#    pasta_a_corrigir.clear()
-
-
-#print(pastas_base)
-#print(pastas_a_corrigir)
